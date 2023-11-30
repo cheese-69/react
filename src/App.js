614cabf2c88f5f0d012d9394b580de146b818c37
cheese-69 <100345558+cheese-69@users.noreply.github.com>
2023-11-30 11:05:54 +0100

    creating my first react app

---
+++ src/App.js
@@ -0,0 +1,65 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import React from "react"
+      export default function App (){
+return( <React.Fragment>
+    <div className='App'>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Navbar</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+      <ul className="navbar-nav">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Features</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link" href="#">Pricing</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link disabled" aria-disabled="true">Disabled</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+<h1>h1. Bootstrap heading</h1>
+<div className="card" style={{width: "18rem"}}>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="card-link">Card link</a>
+    <a href="#" className="card-link">Another link</a>
+  </div>
+  
+</div>
+<div className="card" style={{width: "18rem"}}>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="card-link">Card link</a>
+    <a href="#" className="card-link">Another link</a>
+  </div>
+  
+</div>
+<div className="card" style={{width: "18rem"}}>
+  <div className="card-body">
+    <h5 className="card-title">Card title</h5>
+    <h6 className="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <a href="#" className="card-link">Card link</a>
+    <a href="#" className="card-link">Another link</a>
+  </div>
+  
+</div>
+    </div>
+</React.Fragment>)
+
+
+}
